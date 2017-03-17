a6620ab4aaf5ff0b575b3a43c421e54f33f04f79
Sherif Abdelhamid <sherif@cos.io>
2017-03-17 14:31:07 -0400

    test6

--- addon/helpers/host-app-name.js
+++ addon/helpers/host-app-name.js
@@ -1,6 +1,19 @@
+import Ember from 'ember';
 import config from 'ember-get-config';
 
-export default function() {
+/**
+ Apply the `fix-special-char` utility function to clean up malformed text sent from the server.
 
+ Usage example:
+ ```handlebars
+ This is text we want to fix: {{fix-special-char 'Now &amp; then'}}
+ ```
+
+ @class fix-special-char-helper
+ @uses fix-special-char
+ */
+export function hostAppName() {
     return config.APP.host;
 }
+
+export default Ember.Helper.helper(hostAppName);
