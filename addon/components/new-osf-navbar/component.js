ba61077fce62a6a81e2d8f10c87fe70128e4f53a
Sherif Abdelhamid <sherif@cos.io>
2017-08-30 15:34:18 -0400

    update drop down list

--- addon/components/new-osf-navbar/component.js
+++ addon/components/new-osf-navbar/component.js
@@ -40,6 +40,7 @@ export default Ember.Component.extend(hostAppName, {
             HOME: 'osfHome',
             PREPRINTS: 'preprintsHome',
             REGISTRIES: 'registriesHome',
+            REVIEWS: 'reviewsHome',
             MEETINGS: 'meetingsHome'
         };
         const service = this.get('currentService');
